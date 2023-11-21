399b57b1f15e368225b4f6bff704c62636b906d7
cristian9805 <58344164+cristian9805@users.noreply.github.com>
2023-11-20 23:25:57 -0600

    prueba
    prueba

--- my-app/src/components/Login/Login.js
+++ my-app/src/components/Login/Login.js
@@ -4,7 +4,7 @@ import { jwtDecode } from "jwt-decode";
 
 const Login= () => {
 
-    //CADA UNO AQUI PONE SU ID GENERADO DE GOOGLE CLOUD
+  //CADA UNO AQUI PONE SU ID GENERADO DE GOOGLE CLOUD
   const CLIENT_ID = "186393898515-e9rob8vfk1ntvqjak9j4i1r38gfcfmqt.apps.googleusercontent.com";
   
   const loginSuccessResponseHandler = (credentialResponse) => {
@@ -16,8 +16,10 @@ const Login= () => {
     // redirect a home, props = decoded
   }
 
+
   const loginErrorResponseHandler = () => {
     console.log('Login Failed');
+    console.log('hola')
   }
   
   return (
